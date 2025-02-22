56dda00f7feeb68157f7a01b67c48afbeef1b0c9
J6N6S6 <jonasscandeias@gmail.com>
2025-02-22 20:39:14 -0300

    Add JwtTokenGenerator in TechLibrary.Infrastructure/Security/Tokens/Access

---
+++ TechLibrary.Infrastructure/Security/Tokens/Access/JwtTokenGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using TechLibrary.Infrastructure.Data.Domain.Entities;
+
+namespace TechLibrary.Infrastructure.Security.Tokens.Access
+{
+    public class JwtTokenGenerator
+    {
+        public string Generate(User user)
+        {
+            var tokenDescriptor = new SecurityTokenDescriptor //Precisamos descrever o token
+            {
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey")), SecurityAlgorithms.HmacSha256Signature)
+            };
+        }
+    }
+}
